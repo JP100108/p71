d65d94207a2590e9cb36eb82fc30a22a5cd8dce5
JP100108 <100318527+JP100108@users.noreply.github.com>
2022-11-29 18:36:15 -0300

    Update config.js

--- PRO_1-1_C71_TemplateDoProjeto-main/config.js
+++ PRO_1-1_C71_TemplateDoProjeto-main/config.js
@@ -3,7 +3,12 @@ require("@firebase/firestore");
 
 var firebaseConfig = {
   //Cole sua Configuração do Firebase aqui
-  
+    apiKey: "AIzaSyCxipB4PJwFYPCospz3y5eHUCYHVj41gi8",
+    authDomain: "dsafdsa-8cbff.firebaseapp.com",
+    projectId: "dsafdsa-8cbff",
+    storageBucket: "dsafdsa-8cbff.appspot.com",
+    messagingSenderId: "151354425574",
+    appId: "1:151354425574:web:d74451d4af0a057d273b27"
 };
 
 firebase.initializeApp(firebaseConfig);
